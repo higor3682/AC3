9f7cdf76bc014f26ad8360851aef87e029d93c40
higor <higor.santos@aluno.faculdadeimpacta.com.br>
2020-10-09 21:45:16 -0300

    ac3

--- t8.py
+++ t8.py
@@ -13,6 +13,6 @@ def nao_entre_em_panico():
 
     return primos
 
-if name == "main":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
